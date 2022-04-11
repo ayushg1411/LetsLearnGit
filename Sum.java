1f10cc18f0e4e1242d1ae7e42407ff6b6acf2ace
Ayush Gupta <ayushguptass14@gmail.com>
2022-04-11 21:54:32 +0530

    second commit

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum{
     private static void name() {
         int a=5;
          int b=6;
-         system.out.println(a+b);
+         system.out.println("the sum is" + (a+b));
         
     }
 }
